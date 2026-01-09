26bd467f17cb547445b35268a45e773ddfa4e44e
marcos <marcos.vinicius.dev3@gmail.com>
2026-01-08 22:19:15 -0300

    initial commit

---
+++ src/main/java/marcosvinicius/desafioitau/domain/exceptions/DomainException.java
@@ -0,0 +1,7 @@
+package marcosvinicius.desafioitau.domain.exceptions;
+
+public class DomainException extends RuntimeException {
+    public DomainException(String message) {
+        super(message);
+    }
+}
